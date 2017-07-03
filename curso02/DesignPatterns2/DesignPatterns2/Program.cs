110cfa298222e6ec81fc9d18a83128cdb5afb7e5
Rafael Barros <rafael.rph7@gmail.com>
2017-07-03 09:06:35 -0300

    Factory

---
+++ curso02/DesignPatterns2/DesignPatterns2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection conexao = new ConnectionFactory().getConnection();
+
+            IDbCommand comando = conexao.CreateCommand();
+            comando.CommandText = "SELECT * FROM tabela";
+
+        }
+    }
+}
